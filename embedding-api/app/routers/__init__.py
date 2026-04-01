93f6559d019d551a402dafd60b7385ea959c18dd
Jacob <jhowell45@googlemail.com>
2026-04-02 00:45:09 +0100

    creating healthcheck route and checking it in docker

--- embedding-api/app/routers/__init__.py
+++ embedding-api/app/routers/__init__.py
@@ -1,3 +1,13 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/api")
+
+
+class HealthcheckResponse(BaseModel):
+    ok: bool
+
+
+@router.get("/healthcheck", response_model=HealthcheckResponse)
+async def healthcheck() -> HealthcheckResponse:
+    return HealthcheckResponse(ok=True)
