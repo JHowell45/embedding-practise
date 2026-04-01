e771753312f5f660942ec79033d78b92afad025a
Jacob <jhowell45@googlemail.com>
2026-04-01 23:58:03 +0100

    adding initial root router.

--- embedding-api/app/main.py
+++ embedding-api/app/main.py
@@ -1,8 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI(title="Embedding API")
+from app.routers import router
 
+app = FastAPI(title="Embedding API")
+app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=3000)
